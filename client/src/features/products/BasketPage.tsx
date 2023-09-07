13a450bd41159277bf543944648e0d55bb045eb8
Valery-Klepinin <Valenn1996@mail.ru>
2023-09-07 11:07:05 +0300

    basket

--- client/src/features/products/BasketPage.tsx
+++ client/src/features/products/BasketPage.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
-import ProductItem from './ProductItem';
-import { Order, ProdOrder } from './types/types';
-import { log } from 'console';
+import { Order } from './types/types';
 
 function BasketPage(): JSX.Element {
   const basket: Order = useSelector(
     (store: RootState) => store.products.basket
   );
-  console.log(basket);
 
-  return (
-    <>
-      <div className="products__container">{basket.id}</div>
-
-      {/* {basket.ProdOrders.length > 0 &&
-        basket.ProdOrders.map((prod: ProdOrder) => prod.count)} */}
-    </>
-  );
+  return <div>1</div>;
 }
 
 export default BasketPage;
